7812c79fe9c8cb708ca0678e6a95e27a93998844
Torbjørn Tessem <torbjorn.tessem@nav.no>
2018-08-24 13:05:14 +0200

    [IN-444] Utvidet personinfo-mock og tar i bruk gson for å konvertere data class til json

--- src/main/kotlin/no/nav/personopplysninger_backend/api/rest/person/PersonInformasjonResurs.kt
+++ src/main/kotlin/no/nav/personopplysninger_backend/api/rest/person/PersonInformasjonResurs.kt
@@ -1,5 +1,10 @@
 package no.nav.personopplysninger_backend.api.rest.person
 
+import com.google.gson.GsonBuilder
+import no.nav.personopplysninger_backend.modell.person.Navn
+import no.nav.personopplysninger_backend.modell.person.PersonInformasjon
+import no.nav.personopplysninger_backend.modell.person.Sivilstand
+import no.nav.personopplysninger_backend.modell.person.Telefon
 import no.nav.security.oidc.api.ProtectedWithClaims
 import no.nav.security.oidc.api.Unprotected
 import org.springframework.stereotype.Component
@@ -14,25 +19,51 @@ import javax.ws.rs.Produces
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = arrayOf("acr=Level4"))
 class PersonInformasjonResurs{
 
-    fun mockPersonInformasjon(): Map<String,Any?>{
-        var personinfo = mutableMapOf<String,Any?>()
-        personinfo.put("fornavn","Test")
-        personinfo.put("etternavn","Testesen")
-        personinfo.put("ident","12345678910")
-        return personinfo;
+    val gson = GsonBuilder().setPrettyPrinting().create()
+
+    fun mockPersonInformasjon(): String{
+        val navn: Navn = Navn(
+                "1990.01.01",
+                "",
+                "Prøve",
+                "",
+                "",
+                "Kanin",
+                "")
+
+        val telefon: Telefon = Telefon(
+                "12345678",
+                "2010.01.01",
+                "",
+                "87654321",
+                "2012.01.01",
+                "",
+                "",
+                "",
+                "")
+
+        val sivilstand: Sivilstand = Sivilstand(
+                "1990.01.01",
+                "",
+                "Ugift")
+
+        val personinfo: PersonInformasjon = PersonInformasjon(
+                foedselsdato = "1990.01.01",
+                datoFraOgMed = "1990.01.01",
+                ident = "01019012345",
+                identtype = "fnr",
+                kjonn = "Kvinne",
+                navn = navn,
+                sivilstand = sivilstand,
+                telefon = telefon)
+
+        return gson.toJson(personinfo);
     }
 
-    val hentPersonInformasjon: Map<String,Any?>
+    val hentPersonInformasjon: String
         @GET
         @Produces(MediaType.APPLICATION_JSON)
-        @Path("/personInformasjon")
+        @Path("/personinfo")
         @Unprotected
         get() =  mockPersonInformasjon()
-
-    val hentPerson: String
-        @GET
-        @Path("/person")
-        @Unprotected
-        get() = "Test Testesen"
-
 }
