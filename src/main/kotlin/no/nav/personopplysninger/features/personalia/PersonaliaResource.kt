4f67357da598383f8502bcaca1e59c1fda88c5b3
Jørn Greger Nyegaard-Larsen <Jorn.Greger.Nyegaard-Larsen@nav.no>
2018-11-10 12:09:35 +0100

    Fjernet Gson, da prosjektet alt bruker Jackson via Jersey.
    IN-446. Integrere mot personopplysninger-tjenesten

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaResource.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaResource.kt
@@ -1,23 +1,19 @@
 package no.nav.personopplysninger.features.personalia
 
-import com.google.gson.GsonBuilder
 import no.nav.personopplysninger.features.personalia.model.Personalia
 import no.nav.security.oidc.api.ProtectedWithClaims
 import no.nav.security.oidc.api.Unprotected
 import org.springframework.stereotype.Component
-import javax.ws.rs.core.MediaType
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
-
 
 @Component
 @Path("/personalia")
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = arrayOf("acr=Level4"))
 class PersonaliaResource{
-
-    val gson = GsonBuilder().setPrettyPrinting().create()
 
     val hentPersonalia: Response
         @GET
@@ -36,8 +32,10 @@ class PersonaliaResource{
         @Path("/hent-mock")
         @Unprotected
         get() {
+            val personaliaMock = Personalia("Kari", "Nordmann", "123345678911")
             return Response
-                    .ok(gson.toJson(Personalia("Kari", "Nordmann", "123345678911")))
+                    .ok(personaliaMock)
                     .build()
         }
+
 }
