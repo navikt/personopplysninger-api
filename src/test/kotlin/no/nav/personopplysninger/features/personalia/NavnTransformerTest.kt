44a542c458970f9ac052eae5411c59846ac01a65
Are Husby <are.husby@nav.no>
2018-12-07 13:24:00 +0100

    IN-446 Påbegynt første testklasse

---
+++ src/test/kotlin/no/nav/personopplysninger/features/personalia/NavnTransformerTest.kt
@@ -0,0 +1,17 @@
+package no.nav.personopplysninger.features.personalia
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+
+@TestInstance(PER_CLASS)
+class NavnTransformerTest {
+
+    @Test
+    fun aaaaaa(){
+        Assertions.assertTrue(true)
+    }
+
+
+}
