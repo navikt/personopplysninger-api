8aec64d1d6a24d8b37891d0234139fcb973c8520
Are Husby <ahusby@gmail.com>
2018-12-18 15:14:37 +0100

    IN-701 Legg til object mothers

--- src/test/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaTransformerTest.kt
+++ src/test/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaTransformerTest.kt
@@ -1,18 +1,19 @@
 package no.nav.personopplysninger.features.personalia
 
+import no.nav.tps.person.Personinfo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import kotlin.test.assertTrue
 
 @TestInstance(PER_CLASS)
 class PersonaliaTransformerTest {
 
     @Test
-    fun aaaaa() {
-
+    fun gittPersonalia_skalFaaPersonalia() {
+        val inbound: Personinfo = PersoninfoObjectMother.allFieldsHaveValues
+//        assertTrue(false)
         // TODO Are
-//        val inbound: Personinfo = PersoninfoObjectMother.allFieldsHaveValues
-        //assertTrue(false)
     }
 }
 
