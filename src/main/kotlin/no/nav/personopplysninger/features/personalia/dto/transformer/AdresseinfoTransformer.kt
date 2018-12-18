7ab34d47920a6d265b55f02aa2574c517925057c
Are Husby <ahusby@gmail.com>
2018-12-18 13:32:21 +0100

    IN-701 Refaktorering av bruk av arrow.core.Option

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/dto/transformer/AdresseinfoTransformer.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/dto/transformer/AdresseinfoTransformer.kt
@@ -1,13 +1,12 @@
 package no.nav.personopplysninger.features.personalia.dto.transformer
 
-import arrow.core.Option
-import arrow.core.getOrElse
+import arrow.core.toOption
 import no.nav.tps.person.Adresseinfo
 
 object AdresseinfoTransformer {
     fun toOutbound(inbound: Adresseinfo) = no.nav.personopplysninger.features.personalia.dto.outbound.Adresseinfo(
-            boadresse = Option.fromNullable(inbound.boadresse).map { BoadresseTransformer.toOutbound(it) }.getOrElse { null },
-            postadresse = Option.fromNullable(inbound.postadresse).map { PostadresseTransformer.toOutbound(it) }.getOrElse { null },
-            utenlandskAdresse = Option.fromNullable(inbound.utenlandskAdresse).map { UtenlandskAdresseTransformer.toOutbound(it) }.getOrElse { null }
+            boadresse = inbound.boadresse.toOption().map { BoadresseTransformer.toOutbound(it) }.orNull(),
+            postadresse = inbound.postadresse.toOption().map { PostadresseTransformer.toOutbound(it) }.orNull(),
+            utenlandskAdresse = inbound.utenlandskAdresse.toOption().map { UtenlandskAdresseTransformer.toOutbound(it) }.orNull()
     )
 }
