b5c7abd80047d9e5d350d3baab682d67233cf929
Are Husby <are.husby@nav.no>
2019-01-02 17:50:39 +0100

    IN-701 Litt bedre feilhåndteringer (try-with-resources)

--- src/main/java/no/nav/personopplysninger/features/personalia/PersonConsumer.java
+++ src/main/java/no/nav/personopplysninger/features/personalia/PersonConsumer.java
@@ -5,6 +5,7 @@ import no.nav.tps.person.Personinfo;
 import org.slf4j.MDC;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
@@ -16,31 +17,31 @@ public class PersonConsumer {
     private Client client;
     private URI endpoint;
 
+
     public PersonConsumer(Client client, URI endpoint) {
         this.client = client;
         this.endpoint = endpoint;
     }
 
+
     public Personinfo hentPersonInfo(String fnr) {
-        Response response = client.target(endpoint)
+
+        Invocation.Builder request = client.target(endpoint)
                 .path("person")
                 .request()
                 .header("Nav-Call-Id", MDC.get(MDCConstants.MDC_CORRELATION_ID))
                 .header("Nav-Consumer-Id", CONSUMER_ID)
-                .header("Nav-Personident", fnr)
-                .get();
+                .header("Nav-Personident", fnr);
 
-        if(!SUCCESSFUL.equals(response.getStatusInfo().getFamily())) {
-            throw new RuntimeException(response.readEntity(String.class));
+        try (Response response = request.get()) {
+            if (!SUCCESSFUL.equals(response.getStatusInfo().getFamily())) {
+                // TODO Are: Mer proff feilhåndtering: Logg hva ble forsøkt, hvilken URL ble brukt, fnr/fdato
+                throw new RuntimeException(response.readEntity(String.class));
+            } else {
+                return response.readEntity(Personinfo.class);
+            }
         }
 
-        //Her kalles transformer
-
-        Personinfo dto = response.readEntity(Personinfo.class);
-
-        // TODO: Legg på feilhåndtering
-
-        return dto;
     }
 
 }
