70e2b779077682d7ee8c280a2e4f5a3ad4b0e4a1
Hein Haraldsen <hein.haraldsen@nav.no>
2019-02-19 14:58:04 +0100

    IN-702 Ryddet i configen

--- src/main/java/no/nav/personopplysninger/features/kodeverk/KodeverkConsumer.java
+++ src/main/java/no/nav/personopplysninger/features/kodeverk/KodeverkConsumer.java
@@ -4,9 +4,7 @@ import no.nav.log.MDCConstants;
 import no.nav.personopplysninger.features.kodeverk.exceptions.KodeverkConsumerException;
 import no.nav.personopplysninger.features.personalia.kodeverk.Kjoennstype;
 import org.slf4j.MDC;
-import org.springframework.context.annotation.Bean;
-import no.nav.tjeneste.virksomhet.kodeverk.v2.KodeverkPortType;
-import no.nav.sbl.dialogarena.common.cxf.CXFClient;
+
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Invocation;
@@ -14,12 +12,10 @@ import javax.ws.rs.core.Response;
 import java.net.URI;
 
 import static javax.ws.rs.core.Response.Status.Family.SUCCESSFUL;
-import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createMetricsProxyWithInstanceSwitcher;
 
 public class KodeverkConsumer {
 
     private static final String CONSUMER_ID = "personbruker-personopplysninger-api";
-    public static final String KODEVERK_MOCK_KEY = "kodeverk.personopplysninger.withmock";
     private Client client;
     private URI endpoint;
 
@@ -28,19 +24,6 @@ public class KodeverkConsumer {
         this.endpoint = endpoint;
     }
 
-    @Bean
-    public KodeverkPortType kodeverkPortType() {
-        final KodeverkPortType prod = createKodeverkPortTypeCXFClient().configureStsForSubject().build();
-        return createMetricsProxyWithInstanceSwitcher("kodeverk", prod, prod, KODEVERK_MOCK_KEY, KodeverkPortType.class);
-    }
-
-    private CXFClient<KodeverkPortType> createKodeverkPortTypeCXFClient() {
-        return new CXFClient<>(KodeverkPortType.class)
-                .timeout(15000, 15000)
-                .address(endpoint.toString())
-                .wsdl("classpath:kodeverk/no/nav/tjeneste/virksomhet/kodeverk/v2/Kodeverk.wsdl")
-                .withProperty("ws-security.must-understand", false);
-    }
 
     public Kjoennstype hentKjonn(String kode) {
         Invocation.Builder request = buildRequest(kode);
