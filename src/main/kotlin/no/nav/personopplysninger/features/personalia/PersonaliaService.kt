c587ca3dc724967aeb2173fe0ba752038e56e1b4
Hein Haraldsen <hein.haraldsen@nav.no>
2019-02-22 13:50:58 +0100

    IN-702 Satt opp coden-swagger og Consumer for Kodeverk

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
@@ -1,18 +1,26 @@
 package no.nav.personopplysninger.features.personalia
 
+import no.nav.personopplysninger.features.kodeverk.KodeverkConsumer
 import no.nav.personopplysninger.features.personalia.dto.outbound.PersonaliaOgAdresser
 import no.nav.personopplysninger.features.personalia.dto.transformer.PersonaliaOgAdresserTransformer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.slf4j.LoggerFactory
 
 @Service
 class PersonaliaService @Autowired constructor(
-        private var personConsumer: PersonConsumer
+        private var personConsumer: PersonConsumer,
+        private var kodeverkConsumer: KodeverkConsumer
 ) {
+
+    private val log = LoggerFactory.getLogger(PersonaliaService::class.java)
 
     fun hentPersoninfo(fodselsnr: String): PersonaliaOgAdresser {
         val inbound = personConsumer.hentPersonInfo(fodselsnr)
-        // TODO Are IN-702: Oppslag i kodeverkstjeneste
+        val kjonn = inbound.kjonn
+        log.warn("Kjonn er " + kjonn)
+        val kodeverkkjonn = kodeverkConsumer.hentKjonn(kjonn)
+        log.warn("KodeverkKjonn er " + kodeverkkjonn)
         return PersonaliaOgAdresserTransformer.toOutbound(inbound)
     }
 }
