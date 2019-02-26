0aa81a4d889cf8b64020c6ad38cfe0fe953f1668
Hein Haraldsen <hein.haraldsen@nav.no>
2019-02-26 09:55:03 +0100

    IN-702 Modifisert noarg-constructoren i getKodeverkKoderResponse

--- src/main/java/no/nav/personopplysninger/features/kodeverk/api/GetKodeverkKoderResponse.java
+++ src/main/java/no/nav/personopplysninger/features/kodeverk/api/GetKodeverkKoderResponse.java
@@ -12,8 +12,10 @@ public class GetKodeverkKoderResponse {
     @ApiModelProperty(value = "En liste med alle de eksisterende kodene som tilhører kodeverket.", required = true)
     private final List<String> koder = new ArrayList<>();
 
-    public GetKodeverkKoderResponse () {}
-    
+    public GetKodeverkKoderResponse() {
+        super();
+    }
+
     public GetKodeverkKoderResponse(List<String> koder) {
         this.koder.addAll(koder);
     }
