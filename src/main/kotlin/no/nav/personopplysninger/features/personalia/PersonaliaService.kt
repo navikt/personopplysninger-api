f2b04fcac952724a13df2572d7384e6ba5946996
Hein Haraldsen <hein.haraldsen@nav.no>
2019-02-27 14:52:32 +0100

    IN-702 Debug

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
@@ -1,5 +1,7 @@
 package no.nav.personopplysninger.features.personalia
 
+import arrow.core.left
+import arrow.core.rightIfNotNull
 import no.nav.personopplysninger.features.kodeverk.KodeverkConsumer
 import no.nav.personopplysninger.features.kodeverk.api.Betydning
 import no.nav.personopplysninger.features.kodeverk.api.GetKodeverkKoderBetydningerResponse
@@ -27,7 +29,9 @@ class PersonaliaService @Autowired constructor(
         log.warn("hentetKjonn4 " + hentet.betydninger.values)
         val hentetKjonn = kodeverkConsumer.hentKjonn(inbound.kjonn).betydninger.getValue(inbound.kjonn)[0].beskrivelser
         val test = (hentet.betydninger.get("M"))
-        log.warn("testbetydning " + test);
+        log.warn("testbetydning " + test.isNullOrEmpty());
+        val test2 = (hentet.betydninger.get(inbound.kjonn))
+        log.warn("testbetydning2 " + test2.left());
       //  log.warn("hentetKjonn7 " + hentetKjonn.getValue(inbound.kjonn))
         val land = kodeverkConsumer.hentLandKoder(inbound.foedtILand)
         return PersonaliaOgAdresserTransformer.toOutbound(inbound)
