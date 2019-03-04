a72b205bbe7a673fc8f1d54ea79a4f6e9aea9bdd
Sissel Fladby <sissel.fladby@nav.no>
2019-03-04 13:23:04 +0100

    Legg til endepunkt for DKIF

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
@@ -1,5 +1,6 @@
 package no.nav.personopplysninger.features.personalia
 
+import no.nav.dkif.kontaktinformasjon.DigitalKontaktinfoBolk
 import no.nav.personopplysninger.features.personalia.dto.outbound.PersonaliaOgAdresser
 import no.nav.personopplysninger.features.personalia.dto.transformer.PersonaliaOgAdresserTransformer
 import org.springframework.beans.factory.annotation.Autowired
@@ -7,12 +8,17 @@ import org.springframework.stereotype.Service
 
 @Service
 class PersonaliaService @Autowired constructor(
-        private var personConsumer: PersonConsumer
+        private var personConsumer: PersonConsumer,
+        private var kontaktinfoConsumer: KontaktinfoConsumer
 ) {
 
     fun hentPersoninfo(fodselsnr: String): PersonaliaOgAdresser {
         val inbound = personConsumer.hentPersonInfo(fodselsnr)
         // TODO Are IN-702: Oppslag i kodeverkstjeneste
         return PersonaliaOgAdresserTransformer.toOutbound(inbound)
+    }
+
+    fun hentKontaktinformasjon(fodselsnr: Array<String>): DigitalKontaktinfoBolk {
+        return kontaktinfoConsumer.hentKontaktinformasjon(fodselsnr)
     }
 }
