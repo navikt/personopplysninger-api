81d216045d6f42345c9956189f4f703bd11c851b
Hein Haraldsen <hein.haraldsen@nav.no>
2019-03-05 09:43:44 +0100

    IN-702 Lagt på null-assert på språk

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
@@ -41,7 +41,7 @@ class PersonaliaService @Autowired constructor(
         var personpostnummer = postnummer?.let {postnummer.betydninger.getValue(inbound.adresseinfo?.boadresse?.postnummer)[0]?.beskrivelser}
         var personstatus = status?.let {status.betydninger.getValue(inbound.status?.kode?.verdi)[0]?.beskrivelser}
         var personsivilstand = sivilstand?.let {sivilstand.betydninger.getValue(inbound.sivilstand?.kode?.verdi)[0]?.beskrivelser}
-        var personspraak = inbound.spraak?.let {spraak?.betydninger.getValue(inbound.spraak?.kode?.verdi)[0]?.beskrivelser}
+        var personspraak = inbound.spraak?.let {spraak?.betydninger!!.getValue(inbound.spraak?.kode?.verdi)[0]?.beskrivelser}
         var personstatsborgerskap = statsborgerskap?.let {statsborgerskap.betydninger.getValue(inbound.statsborgerskap?.kode?.verdi)[0]?.beskrivelser}
 
         val kjonnterm = personkjonn?.getValue(kodeverkspraak)?.term
@@ -63,7 +63,7 @@ class PersonaliaService @Autowired constructor(
         personaliaKodeverk.spraakterm = spraakterm
         personaliaKodeverk.stasborgerskapterm = statsborgerskapterm
         personaliaKodeverk.statusterm = statusterm
-        
+
         return PersonaliaOgAdresserTransformer.toOutbound(inbound, personaliaKodeverk)
     }
 }
