730d653972ac701f1edb8640423b35b1c56b139a
Hein Haraldsen <hein.haraldsen@nav.no>
2019-03-08 09:49:38 +0100

    IN-702 Debug

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaService.kt
@@ -35,6 +35,7 @@ class PersonaliaService @Autowired constructor(
         val spraak = kodeverkConsumer.hentSpraak(inbound.spraak?.kode?.verdi)
         val statsborgerskap = kodeverkConsumer.hentStatsborgerskap(inbound.statsborgerskap?.kode?.verdi)
         log.error("foedIkommune " + inbound.foedtIKommune?.verdi  + "land " + land + " " + bostedskommune)
+        log.error("foedIkommune " + inbound.adresseinfo?.boadresse?.kommune.isNullOrBlank())
         val personkjonn = (!inbound.kjonn.isNullOrBlank()).let {kjonn.betydninger.getValue(inbound.kjonn)[0]?.beskrivelser }
         val personfoedtkommune = (!inbound.foedtIKommune?.verdi.isNullOrBlank()).let {foedtkommune.betydninger.getValue(inbound.foedtIKommune?.verdi)[0]?.beskrivelser}
         val personbostedskommune = (!inbound.adresseinfo?.boadresse?.kommune.isNullOrBlank()).let {bostedskommune.betydninger.getValue(inbound.adresseinfo?.boadresse?.kommune)[0]?.beskrivelser}
