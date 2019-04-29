a299d149e9bdb584469e19940cac6bcebc8089bd
Hein Haraldsen <hein.haraldsen@nav.no>
2019-04-29 10:45:28 +0200

    Endret på appdynamics-configen

--- appdynamics.sh
+++ appdynamics.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-export APPD_NAME="my-prefix-${APP_NAME}"
-export APPD_TIER=backend
-export APPD_HOSTNAME="$FASIT_ENVIRONMENT_NAME-$HOSTNAME"
+if [ -z ${APPDYNAMICS+x} ]; then
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/appdynamics/javaagent.jar "
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=${APP_NAME}_${FASIT_ENVIRONMENT_NAME}"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}_"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+    export JAVA_OPTS
+fi
