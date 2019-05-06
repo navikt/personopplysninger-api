681ffca8c3a5d8490ac5b9f761311c6c89c27501
Hein Haraldsen <hein.haraldsen@nav.no>
2019-05-06 17:36:04 +0200

    Lagt på appconfig i cache

--- src/main/java/no/nav/personopplysninger/api/Launcher.java
+++ src/main/java/no/nav/personopplysninger/api/Launcher.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@EnableCaching
 public class Launcher {
 
     public static void main(String... args) {
