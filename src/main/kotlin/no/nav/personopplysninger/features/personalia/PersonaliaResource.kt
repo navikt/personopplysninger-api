cee1e5c6796a32e72c05404b51dda3f171669ced
heinharaldsen <heinharaldsen@users.noreply.github.com>
2019-05-16 12:47:22 +0200

    Feature/in 1075 (#53)
    * IN-1075 Oppdatert common-java-modules
    
    * IN-1075 Byttet ut no.nav.sbl.log med no.nav.common.log
    
    * IN-1075 Fjernet fil som ikke skulle sjekkes inn
    
    * IN-1075 Lagt på cacheEvict
    
    * IN-1075 Lagt på cache-control på responsen fra personalia
    
    * IN-1075 Ryddet opp i pom.xml
    
    * IN-1075 Ryddet i imports

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaResource.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/PersonaliaResource.kt
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Component
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.core.CacheControl
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
 private const val claimsIssuer = "selvbetjening"
+private val cacheControl = CacheControl()
 
 @Component
 @Path("/")
@@ -21,10 +23,13 @@ class PersonaliaResource @Autowired constructor(private var personaliaService: P
     @Path("/personalia")
     @Produces(MediaType.APPLICATION_JSON)
     fun hentPersoninfo(): Response {
+        cacheControl.setMustRevalidate(true)
+        cacheControl.setNoStore(true)
         val fodselsnr = hentFnrFraToken()
         val personaliaOgAdresser = personaliaService.hentPersoninfo(fodselsnr)
         return Response
                 .ok(personaliaOgAdresser)
+                .cacheControl(cacheControl)
                 .build()
     }
 
