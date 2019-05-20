c455d16dc5fdffaa07cd52b12f963ffa3c95eee4
Hein Haraldsen <hein.haraldsen@nav.no>
2019-05-20 13:59:53 +0200

    IN-765 Lagt på offentligIdent på arbeidsgiver

--- src/main/kotlin/no/nav/personopplysninger/features/arbeidsforhold/domain/Arbeidsgiver.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/arbeidsforhold/domain/Arbeidsgiver.kt
@@ -3,6 +3,7 @@ package no.nav.personopplysninger.features.arbeidsforhold.domain
 data class Arbeidsgiver (
 
         val organisasjonsnummer: kotlin.String? = null,
-        val type: kotlin.String? = null
+        val type: kotlin.String? = null,
+        val offentlingIdent: kotlin.String? = null
 
 )
