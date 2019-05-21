49dd369d73f7adeb8f248108d6da6dbb33b52ecc
Hein Haraldsen <hein.haraldsen@nav.no>
2019-05-21 16:19:11 +0200

    IN-765 Lagt på kall for enkelt arbeidsforhold

--- src/main/kotlin/no/nav/personopplysninger/features/arbeidsforhold/ArbeidsforholdIdResource.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/arbeidsforhold/ArbeidsforholdIdResource.kt
@@ -5,8 +5,12 @@ import no.nav.security.oidc.api.ProtectedWithClaims
 import no.nav.security.oidc.jaxrs.OidcRequestContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
@@ -14,21 +18,21 @@ import javax.ws.rs.core.Response
 private const val claimsIssuer = "selvbetjening"
 
 @Component
-@Path("/arbeidsforholdinnslag")
+@Path("/arbeidsforholdinnslag/{id}")
 @ProtectedWithClaims(issuer = claimsIssuer, claimMap = ["acr=Level4"])
 class ArbeidsforholdIdResource @Autowired constructor(private var arbeidsforholdIdService: ArbeidsforholdService) {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    fun hentPersonalia(): Response {
+    fun hentPersonalia(@PathParam("id") id : String): Response {
         val fodselsnr = hentFnrFraToken()
-        val id = hentIdFraAttribute().toInt()
         val fssToken = hentFssToken()
-        val arbeidsforhold = arbeidsforholdIdService.hentEttArbeidsforholdmedId(fodselsnr, id, fssToken)
+        val arbeidsforhold = arbeidsforholdIdService.hentEttArbeidsforholdmedId(fodselsnr, id.toInt(), fssToken)
         return Response
                 .ok(arbeidsforhold)
                 .build()
     }
+
 
     private fun hentFssToken(): String {
         return arbeidsforholdIdService.hentFSSToken()
@@ -39,8 +43,5 @@ class ArbeidsforholdIdResource @Autowired constructor(private var arbeidsforhold
         return context.getClaims(claimsIssuer).claimSet.subject
     }
 
-    private fun hentIdFraAttribute(): String {
-        val id = OidcRequestContext.getHolder().getRequestAttribute("id")
-        return id.toString()
-    }
+
 }
