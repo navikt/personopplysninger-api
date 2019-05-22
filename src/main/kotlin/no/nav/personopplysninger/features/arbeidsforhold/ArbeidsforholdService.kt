94e243108fd6545327ba611e96436d0634cec456
Hein Haraldsen <hein.haraldsen@nav.no>
2019-05-22 14:02:34 +0200

    IN-765 Lagt på logging på ereg

--- src/main/kotlin/no/nav/personopplysninger/features/arbeidsforhold/ArbeidsforholdService.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/arbeidsforhold/ArbeidsforholdService.kt
@@ -30,8 +30,9 @@ class ArbeidsforholdService @Autowired constructor(
         val inbound = arbeidsforholdConsumer.hentArbeidsforholdmedFnr(fodselsnr, fssToken)
         var arbeidsforholdDtos = mutableListOf<ArbeidsforholdDto>()
         for (af in inbound) {
-            val arbgivnavn = eregConsumer.hentOrgnavn(af.arbeidsgiver?.organisasjonsnummer).redigertnavn
-            arbeidsforholdDtos.add(ArbeidsforholdTransformer.toOutbound(af, arbgivnavn))
+            log.warn("Eregoppslag " + af.arbeidsgiver?.organisasjonsnummer)
+       //    val arbgivnavn = eregConsumer.hentOrgnavn(af.arbeidsgiver?.organisasjonsnummer).redigertnavn
+            arbeidsforholdDtos.add(ArbeidsforholdTransformer.toOutbound(af, ""))
         }
         return arbeidsforholdDtos
     }
@@ -40,8 +41,9 @@ class ArbeidsforholdService @Autowired constructor(
         val inbound = arbeidsforholdConsumer.hentArbeidsforholdmedId(fodselsnr, id, fssToken)
         var arbeidsforholdDtos = mutableListOf<ArbeidsforholdDto>()
         for (af in inbound) {
-            val arbgivnavn = eregConsumer.hentOrgnavn(af.arbeidsgiver?.organisasjonsnummer).redigertnavn
-            arbeidsforholdDtos.add(ArbeidsforholdTransformer.toOutbound(af, arbgivnavn))
+            log.warn("Eregoppslag " + af.arbeidsgiver?.organisasjonsnummer)
+   //         val arbgivnavn = eregConsumer.hentOrgnavn(af.arbeidsgiver?.organisasjonsnummer).redigertnavn
+            arbeidsforholdDtos.add(ArbeidsforholdTransformer.toOutbound(af, ""))
         }
         return arbeidsforholdDtos
 
