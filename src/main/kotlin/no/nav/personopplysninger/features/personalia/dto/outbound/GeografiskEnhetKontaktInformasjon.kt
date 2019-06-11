856744370956043f4e63d805e7d374bb6102a9b7
Hein Haraldsen <hein.haraldsen@nav.no>
2019-06-11 09:03:07 +0200

    IN-895 Fjernet overflødige felter

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/dto/outbound/GeografiskEnhetKontaktInformasjon.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/dto/outbound/GeografiskEnhetKontaktInformasjon.kt
@@ -2,10 +2,7 @@ package no.nav.personopplysninger.features.personalia.dto.outbound
 
 
 data class GeografiskEnhetKontaktInformasjon (
-        val gateadresse: String? = null,
-        val poststed: String? = null,
         val publikumsmottak: List<PublikumsmottakDto>,
-        val andre: String? = null,
         val tlfperson: String? = null,
         val spesielleopplysninger: String? = null
 )
