631c776b2067a4625a4ddd713628bcf6626996b6
Øyvind Hagen <oyvind.hagen@nav.no>
2019-09-02 15:08:58 +0200

    Lagt til JSON-creator.

--- src/main/java/no/nav/personopplysninger/features/endreopplysninger/domain/kontonummer/Kontonummer.java
+++ src/main/java/no/nav/personopplysninger/features/endreopplysninger/domain/kontonummer/Kontonummer.java
@@ -1,12 +1,23 @@
 package no.nav.personopplysninger.features.endreopplysninger.domain.kontonummer;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Kontonummer {
     private String kilde = "BRUKER SELV";
     private UtenlandskKontoInformasjon utenlandskKontoInformasjon;
     private String value;
+
+    @JsonCreator
+    public static Kontonummer create(String json) throws JsonParseException, JsonMappingException, IOException {
+        return (new ObjectMapper()).readValue(json, Kontonummer.class);
+    }
 
     public String getKilde() {
         return kilde;
