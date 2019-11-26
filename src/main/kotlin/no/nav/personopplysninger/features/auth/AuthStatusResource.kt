2f93c600c48fee415b9d7bf71ea9e61032c47395
Øyvind Hagen <oyvind.hagen@nav.no>
2019-11-26 09:27:14 +0100

    Returnerer json på auth-endepunkt.

--- src/main/kotlin/no/nav/personopplysninger/features/auth/AuthStatusResource.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/auth/AuthStatusResource.kt
@@ -5,14 +5,17 @@ import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.Produces
 import javax.ws.rs.core.Context
+import javax.ws.rs.core.MediaType
 
 @Component
 @Path("auth")
 class AuthStatusResource {
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Unprotected
     fun isAuthenticatedWithOIDC(@Context request: HttpServletRequest): String {
-        return request.cookies?.any { cookie -> cookie.name.equals("selvbetjening-idtoken") }.toString().toLowerCase()
+        return """{"authorized":"${request.cookies?.any { cookie -> cookie.name.equals("selvbetjening-idtoken") }.toString().toLowerCase()}"}"""
     }
 }
