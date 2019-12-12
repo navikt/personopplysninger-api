b05de9e79f25a3b116b1f42846fe33ab7de96a68
Øyvind Hagen <oyvind.hagen@nav.no>
2019-12-12 12:50:53 +0100

    Lagt til JavaTimeModule til objectmapper.

--- src/main/kotlin/no/nav/personopplysninger/consumerutils/responseReader.kt
+++ src/main/kotlin/no/nav/personopplysninger/consumerutils/responseReader.kt
@@ -1,5 +1,6 @@
 package no.nav.personopplysninger.consumerutils
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import javax.ws.rs.ProcessingException
 import javax.ws.rs.core.Response
@@ -18,11 +19,13 @@ inline fun <reified T> Response.unmarshalBody(): T {
 
 inline fun <reified T> Response.unmarshalList(): List<T> {
     return try {
-        jacksonObjectMapper().run {
-            val json = readEntity(String::class.java)
-            val type = typeFactory.constructCollectionLikeType(List::class.java, T::class.java)
-            readValue(json, type)
-        }
+        jacksonObjectMapper()
+                .registerModule(JavaTimeModule())
+                .run {
+                    val json = readEntity(String::class.java)
+                    val type = typeFactory.constructCollectionLikeType(List::class.java, T::class.java)
+                    readValue(json, type)
+                }
     } catch (e: ProcessingException) {
         throw ConsumerException("Prosesseringsfeil på responsobjekt. Responsklasse: ", e);
     } catch (e: IllegalStateException) {
@@ -34,10 +37,12 @@ inline fun <reified T> Response.unmarshalList(): List<T> {
 
 fun <T> Response.unmarshalList(responsklasse: Class<T>): List<T> {
     return try {
-        jacksonObjectMapper().run {
-            val type = typeFactory.constructCollectionType(List::class.java, responsklasse)
-            readValue(readEntity(String::class.java), type)
-        }
+        jacksonObjectMapper()
+                .registerModule(JavaTimeModule())
+                .run {
+                    val type = typeFactory.constructCollectionType(List::class.java, responsklasse)
+                    readValue(readEntity(String::class.java), type)
+                }
     } catch (e: ProcessingException) {
         throw ConsumerException("Prosesseringsfeil på responsobjekt. Responsklasse: ", e)
     } catch (e: IllegalStateException) {
