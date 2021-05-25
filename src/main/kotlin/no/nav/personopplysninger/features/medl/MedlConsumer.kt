acc49df03d402fba6e32d404fdce120cf7381bc4
AudunSBerget <audun.svensson.berget@nav.no>
2021-05-25 10:04:49 +0200

    Use tokenExchange

--- src/main/kotlin/no/nav/personopplysninger/features/medl/MedlConsumer.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/medl/MedlConsumer.kt
@@ -3,6 +3,7 @@ package no.nav.personopplysninger.features.medl
 import no.nav.log.MDCConstants
 import no.nav.personopplysninger.consumerutils.*
 import no.nav.personopplysninger.features.medl.domain.Medlemskapsunntak
+import no.nav.personopplysninger.features.tokenx.TokenDingsService
 import no.nav.personopplysninger.oppslag.sts.STSConsumer
 import org.slf4j.MDC
 import java.net.URI
@@ -13,12 +14,15 @@ import javax.ws.rs.core.Response.Status.Family.SUCCESSFUL
 class MedlConsumer constructor(
         private val client: Client,
         private val endpoint: URI,
-        private val stsConsumer: STSConsumer
+        private val stsConsumer: STSConsumer,
+        private val tokenDingsService: TokenDingsService
 )
 {
     fun hentMedlemskap(fnr: String): Medlemskapsunntak {
         try {
-            val response = getBuilder(fnr).get()
+            val tokenx = tokenDingsService.exchangeToken()
+            val response = getBuilder(fnr, tokenx.accessToken).get()
+
             if (!SUCCESSFUL.equals(response.statusInfo.family)) {
                 val msg = "Forsøkte å konsumere REST-tjenesten medl. endpoint=[$endpoint], HTTP response status=[${response.status}]. "
                 throw ConsumerException(msg.plus(response.unmarshalBody()))
@@ -30,12 +34,12 @@ class MedlConsumer constructor(
         }
     }
 
-    private fun getBuilder(fnr: String): Invocation.Builder {
+    private fun getBuilder(fnr: String, accessToken: String): Invocation.Builder {
         return client.target(endpoint)
                 .path("v1/innsyn/person")
                 .request()
                 .header(HEADER_NAV_CALL_ID, MDC.get(MDCConstants.MDC_CALL_ID))
-                .header(HEADER_NAV_CONSUMER_TOKEN, "Bearer $systemToken")
+                .header(HEADER_NAV_CONSUMER_TOKEN, "Bearer $accessToken")
                 .header(HEADER_NAV_CONSUMER_ID, CONSUMER_ID)
                 .header(HEADER_NAV_PERSONIDENT_KEY, fnr)
     }
