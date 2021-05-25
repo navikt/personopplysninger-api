686afd540eb37e720830530a15185c4fe88be4b9
AudunSBerget <audun.svensson.berget@nav.no>
2021-05-25 10:39:25 +0200

    Add missing parameter

--- src/main/kotlin/no/nav/personopplysninger/features/medl/MedlConfiguration.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/medl/MedlConfiguration.kt
@@ -2,6 +2,7 @@ package no.nav.personopplysninger.features.medl
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.personopplysninger.consumerutils.DEFAULT_APIKEY_USERNAME
+import no.nav.personopplysninger.features.tokenx.TokenDingsService
 import no.nav.personopplysninger.oppslag.sts.STSConsumer
 import no.nav.security.token.support.jaxrs.JwtTokenClientRequestFilter
 import org.springframework.beans.factory.annotation.Value
@@ -26,8 +27,9 @@ open class MedlConfiguration {
     open fun medlConsumer(
             @Named("medlClient") client: Client,
             @Value("\${MEDL2_API_URL}") medlUri: String,
-            stsConsumer: STSConsumer): MedlConsumer{
-        return MedlConsumer(client, URI(medlUri), stsConsumer)
+            stsConsumer: STSConsumer,
+            tokenDingsService: TokenDingsService): MedlConsumer {
+        return MedlConsumer(client, URI(medlUri), stsConsumer, tokenDingsService)
     }
 
     @Bean
