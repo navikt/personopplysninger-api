3591c06fea89a7c18d7c63253b2c5304a93e695c
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2021-12-03 16:07:20 +0100

    Fikset mapping av fødested

--- src/main/kotlin/no/nav/personopplysninger/features/personalia/dto/transformer/PersoninfoTransformer.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/personalia/dto/transformer/PersoninfoTransformer.kt
@@ -34,7 +34,7 @@ object PersoninfoTransformer {
     }
 
     private fun foedested(foedtIKommune: String?, foedtILand: String?): String? {
-        val names = listOfNotNull(foedtIKommune, foedtILand)
+        val names = listOfNotNull(foedtIKommune, foedtILand).filter { it.isNotEmpty() }
         return if (names.isEmpty()) null else names.joinToString(", ")
     }
 }
