57131cb534b7abbdf171f649829474d4093f09d7
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-01-24 12:31:19 +0100

    Lagt til exception handler for web application exceptions

--- src/main/kotlin/no/nav/personopplysninger/exception/WebApplicationExceptionMapper.kt
+++ src/main/kotlin/no/nav/personopplysninger/exception/WebApplicationExceptionMapper.kt
@@ -2,17 +2,18 @@ package no.nav.personopplysninger.exception
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
 
 @Provider
-class ExceptionHandler: ExceptionMapper<Throwable> {
+class WebApplicationExceptionMapper: ExceptionMapper<WebApplicationException> {
 
     private var logger: Logger = LoggerFactory.getLogger(javaClass)
 
-    override fun toResponse(e: Throwable): Response {
+    override fun toResponse(e: WebApplicationException): Response {
         logger.error(e.message, e)
-        return Response.serverError().build()
+        return e.response
     }
 }
