19c55afb52441e3d9039534cb40fd3be301cc6e2
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-03-08 10:24:35 +0100

    Henter fnr fra pid-claim i stedet for sub-claim

--- src/main/kotlin/no/nav/personopplysninger/features/auth/AuthStatusResource.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/auth/AuthStatusResource.kt
@@ -1,11 +1,11 @@
 package no.nav.personopplysninger.features.auth
 
 import no.nav.personopplysninger.features.personalia.pdl.PdlConsumer
+import no.nav.personopplysninger.util.hentFnrFraToken
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.security.token.support.jaxrs.JaxrsTokenValidationContextHolder
+import org.apache.commons.lang3.StringUtils.isEmpty
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import org.springframework.util.StringUtils.isEmpty
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -15,20 +15,15 @@ private const val claimsIssuer = "selvbetjening"
 
 @Component
 @Path("name")
-@ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
-class AuthStatusResource  @Autowired constructor(private var pdlConsumer: PdlConsumer) {
+@ProtectedWithClaims(issuer = claimsIssuer, claimMap = ["acr=Level4"])
+class AuthStatusResource @Autowired constructor(private var pdlConsumer: PdlConsumer) {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     fun getName(): Map<String, String> {
-        val navn = pdlConsumer.getNavn(fnr).navn.firstOrNull()
+        val navn = pdlConsumer.getNavn(hentFnrFraToken()).navn.firstOrNull()
         val fulltNavn = listOf(navn?.fornavn, navn?.mellomnavn, navn?.etternavn).filter { !isEmpty(it) }
             .joinToString(separator = " ")
         return mapOf("name" to fulltNavn)
-    }
-
-    private inline val fnr: String get() {
-        val context = JaxrsTokenValidationContextHolder.getHolder()
-        return context.tokenValidationContext.getClaims(claimsIssuer).subject
     }
 }
