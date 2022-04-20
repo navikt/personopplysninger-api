23de90ccce9cc1dd09e7a07cdd2377be03f88b74
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-04-20 12:24:23 +0200

    Oppdaterer alle felt i utenlandsk bankinfo

--- src/main/kotlin/no/nav/personopplysninger/features/endreopplysninger/EndreOpplysningerService.kt
+++ src/main/kotlin/no/nav/personopplysninger/features/endreopplysninger/EndreOpplysningerService.kt
@@ -52,9 +52,14 @@ class EndreOpplysningerService(
             nyttKontonummer = kontonummer.value,
             utenlandskKontoInfo = kontonummer.utenlandskKontoInformasjon?.let {
                 UtenlandskKontoInfo(
+                    banknavn = kontonummer.utenlandskKontoInformasjon.bank?.navn,
+                    bankkode = kontonummer.utenlandskKontoInformasjon.bank?.kode,
                     bankLandkode = kontonummer.utenlandskKontoInformasjon.landkode,
                     valutakode = kontonummer.utenlandskKontoInformasjon.valuta!!,
-                    swiftBicKode = kontonummer.utenlandskKontoInformasjon.swift
+                    swiftBicKode = kontonummer.utenlandskKontoInformasjon.swift,
+                    bankadresse1 = kontonummer.utenlandskKontoInformasjon.bank?.adresseLinje1,
+                    bankadresse2 = kontonummer.utenlandskKontoInformasjon.bank?.adresseLinje2,
+                    bankadresse3 = kontonummer.utenlandskKontoInformasjon.bank?.adresseLinje3,
                 )
             }
         )
