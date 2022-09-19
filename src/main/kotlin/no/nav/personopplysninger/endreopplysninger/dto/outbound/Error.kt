7e6b83b0bb6ba53bb4b5801f1324a42bf063e85a
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-09-19 09:08:45 +0200

    Fjernet unødvendig logging og ryddet litt opp i kode

--- src/main/kotlin/no/nav/personopplysninger/endreopplysninger/dto/outbound/Error.kt
+++ src/main/kotlin/no/nav/personopplysninger/endreopplysninger/dto/outbound/Error.kt
@@ -3,7 +3,7 @@ package no.nav.personopplysninger.endreopplysninger.dto.outbound
 import kotlinx.serialization.Serializable
 
 @Serializable
-class Error {
-    var message: String? = null
+data class Error(
+    var message: String? = null,
     val details: Map<String, List<String>>? = null
-}
+)
