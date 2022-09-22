7ec01c2e6ebc58a60656754e8067b1abbe259c31
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-09-22 09:56:30 +0200

    Lagt til namespace

--- src/main/kotlin/no/nav/personopplysninger/config/MetricsCollector.kt
+++ src/main/kotlin/no/nav/personopplysninger/config/MetricsCollector.kt
@@ -4,13 +4,17 @@ import io.prometheus.client.Counter
 
 object MetricsCollector {
 
+    private const val NAMESPACE = "personbruker"
+
     val NORSK_KONTONUMMER_COUNTER = Counter.build()
         .name("endring_norsk_kontonummer")
+        .namespace(NAMESPACE)
         .help("Antall endringer av norske kontonummer")
         .register()
 
     val UTENLANDSK_KONTONUMMER_COUNTER = Counter.build()
         .name("endring_utenlandsk_kontonummer")
+        .namespace(NAMESPACE)
         .help("Antall endringer av utenlandske kontonummer")
         .register()
 }
