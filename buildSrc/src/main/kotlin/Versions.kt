d1a7d4f451e38ec1e63b69dc26821b5642d6fb05
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-11-01 13:44:15 +0100

    Bumpet dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Auth0 {
-    private const val version = "4.1.0"
+    private const val version = "4.2.1"
     private const val groupId = "com.auth0"
 
     const val jwt = "$groupId:java-jwt:$version"
@@ -28,7 +28,7 @@ object Kotlin {
 }
 
 object Ktor {
-    private const val version = "2.1.2"
+    private const val version = "2.1.3"
     private const val groupId = "io.ktor"
 
     const val metricsMicrometer = "$groupId:ktor-server-metrics-micrometer:$version"
@@ -71,14 +71,14 @@ object Mockk {
 
 object NAV {
     object Security {
-        private const val version = "2.1.6"
+        private const val version = "2.1.7"
         private const val groupId = "no.nav.security"
 
         const val tokenValidationKtor = "$groupId:token-validation-ktor-v2:$version"
     }
 
     object Common {
-        private const val version = "2.2022.10.20_07.46-64a2e6bdf2d0"
+        private const val version = "2.2022.10.28_08.02-d7d341cf26ba"
         private const val groupId = "no.nav.common"
         const val featureToggle = "$groupId:feature-toggle:$version"
     }
