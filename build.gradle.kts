2bc7501389fa4665fae5905af55a781af1e27e35
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-03-05 13:58:32 +0100

    Refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.allopen") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
+    val kotlinVersion = "1.9.22"
+    val shadowVersion = "8.1.1"
+    val versionsVersion = "0.51.0"
 
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("com.github.ben-manes.versions") version "0.51.0" // ./gradlew dependencyUpdates to check for new versions
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.allopen") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("com.github.johnrengelman.shadow") version shadowVersion
+    id("com.github.ben-manes.versions") version versionsVersion // ./gradlew dependencyUpdates to check for new versions
     application
 }
 
@@ -25,20 +29,20 @@ repositories {
     }
 }
 
-val auth0JwtVersion = "4.4.0"
-val caffeineVersion = "3.1.8"
-val kafkaVersion = "3.7.0"
-val ktorVersion = "2.3.9"
-val logbackVersion = "1.5.3"
-val logstashVersion = "7.4"
-val micrometerVersion = "1.12.3"
-val mockkVersion = "1.13.10"
-val navSecurityVersion = "4.1.3"
-val kotlinVersion = "1.9.22"
-val tmsKtorTokenSupportVersion = "4.0.0"
-val tmsVarselBuilderVersion = "1.0.2"
-
 dependencies {
+    val auth0JwtVersion = "4.4.0"
+    val caffeineVersion = "3.1.8"
+    val kafkaVersion = "3.7.0"
+    val ktorVersion = "2.3.9"
+    val logbackVersion = "1.5.3"
+    val logstashVersion = "7.4"
+    val micrometerVersion = "1.12.3"
+    val mockkVersion = "1.13.10"
+    val navSecurityVersion = "4.1.3"
+    val kotlinVersion = "1.9.22"
+    val tmsKtorTokenSupportVersion = "4.0.0"
+    val tmsVarselBuilderVersion = "1.0.2"
+
     implementation("com.auth0:java-jwt:$auth0JwtVersion")
     implementation("com.github.ben-manes.caffeine:caffeine:$caffeineVersion")
     implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
