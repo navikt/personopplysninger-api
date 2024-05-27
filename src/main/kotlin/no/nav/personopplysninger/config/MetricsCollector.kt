530984e3ada279a4bcbba34489e6939b42e0b91e
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-05-27 14:10:36 +0200

    Fikset tester

--- src/main/kotlin/no/nav/personopplysninger/config/MetricsCollector.kt
+++ src/main/kotlin/no/nav/personopplysninger/config/MetricsCollector.kt
@@ -7,26 +7,31 @@ class MetricsCollector(registry: PrometheusRegistry) {
     val endreTelefonnummerCounter: Counter = Counter.builder()
         .name("${NAMESPACE}_endring_telefonnummer")
         .help("Antall endringer av telefonnummer")
+        .withoutExemplars()
         .register(registry)
 
     val slettTelefonnummerCounter: Counter = Counter.builder()
         .name("${NAMESPACE}_sletting_telefonnummer")
         .help("Antall slettinger av telefonnummer")
+        .withoutExemplars()
         .register(registry)
 
     val endreNorskKontonummerCounter: Counter = Counter.builder()
         .name("${NAMESPACE}_endring_norsk_kontonummer")
         .help("Antall endringer av norske kontonummer")
+        .withoutExemplars()
         .register(registry)
 
     val endreUtenlandskKontonummerCounter: Counter = Counter.builder()
         .name("${NAMESPACE}_endring_utenlandsk_kontonummer")
         .help("Antall endringer av utenlandske kontonummer")
+        .withoutExemplars()
         .register(registry)
 
     val slettKontaktadresseCounter: Counter = Counter.builder()
         .name("${NAMESPACE}_sletting_kontaktadresse")
         .help("Antall slettinger av kontaktadresser")
+        .withoutExemplars()
         .register(registry)
 
     companion object {
