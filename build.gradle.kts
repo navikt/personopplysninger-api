fbe3c4121c5d8d85a05cabb21229e327b747cb1c
Skifjeld <eivind.skifjeld@nav.no>
2025-02-24 14:26:20 +0100

    Oppdatert avhengigheter 240225 ingen major

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 plugins {
     val kotlinVersion = "2.1.0"
     val shadowVersion = "8.1.1"
-    val versionsVersion = "0.51.0"
+    val versionsVersion = "0.52.0"
     val graphqlVersion = "8.2.1"
 
     kotlin("jvm") version kotlinVersion
@@ -31,15 +31,15 @@ repositories {
 }
 
 dependencies {
-    val auth0JwtVersion = "4.4.0"
-    val caffeineVersion = "3.1.8"
-    val graphqlVersion = "8.2.1"
+    val auth0JwtVersion = "4.5.0"
+    val caffeineVersion = "3.2.0"
+    val graphqlVersion = "8.3.0"
     val kafkaVersion = "3.9.0"
-    val ktorVersion = "3.0.3"
-    val logbackVersion = "1.5.13"
+    val ktorVersion = "3.1.0"
+    val logbackVersion = "1.5.16"
     val logstashVersion = "8.0"
-    val micrometerVersion = "1.14.2"
-    val mockkVersion = "1.13.13"
+    val micrometerVersion = "1.14.4"
+    val mockkVersion = "1.13.16"
     val navSecurityVersion = "5.0.13"
     val kotlinVersion = "2.1.0"
     val tmsKtorTokenSupportVersion = "5.0.1"
